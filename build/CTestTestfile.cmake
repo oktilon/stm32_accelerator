36e11f60f4f0953a87e823844ca9549a8a90bc64
Denys Stovbun <denis.stovbun@lanars.com>
2022-08-23 14:55:37 +0300

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/denis/projects/stm32/acc1
+# Build directory: /home/denis/projects/stm32/acc1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
